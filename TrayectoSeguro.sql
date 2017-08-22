1f13b590315d3328c5930440574d5c4f652f36f7
Arley Duarte <arleymauricio@gmail.com>
2017-08-22 14:55:25 +0000

    TrayectoSeguro.sql created online with Bitbucket

---
+++ TrayectoSeguro.sql
@@ -0,0 +1,684 @@
+USE [master]
+GO
+/****** Object:  Database [trayectoseguro]    Script Date: 8/22/2017 10:54:48 AM ******/
+CREATE DATABASE [trayectoseguro]
+GO
+ALTER DATABASE [trayectoseguro] SET COMPATIBILITY_LEVEL = 130
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [trayectoseguro].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [trayectoseguro] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [trayectoseguro] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET ALLOW_SNAPSHOT_ISOLATION ON 
+GO
+ALTER DATABASE [trayectoseguro] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [trayectoseguro] SET READ_COMMITTED_SNAPSHOT ON 
+GO
+ALTER DATABASE [trayectoseguro] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET  MULTI_USER 
+GO
+ALTER DATABASE [trayectoseguro] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [trayectoseguro] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [trayectoseguro] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 7), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 10, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO)
+GO
+USE [trayectoseguro]
+GO
+/****** Object:  UserDefinedFunction [dbo].[fn_diagramobjects]    Script Date: 8/22/2017 10:54:48 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+	CREATE FUNCTION [dbo].[fn_diagramobjects]() 
+	RETURNS int
+	WITH EXECUTE AS N'dbo'
+	AS
+	BEGIN
+		declare @id_upgraddiagrams		int
+		declare @id_sysdiagrams			int
+		declare @id_helpdiagrams		int
+		declare @id_helpdiagramdefinition	int
+		declare @id_creatediagram	int
+		declare @id_renamediagram	int
+		declare @id_alterdiagram 	int 
+		declare @id_dropdiagram		int
+		declare @InstalledObjects	int
+
+		select @InstalledObjects = 0
+
+		select 	@id_upgraddiagrams = object_id(N'dbo.sp_upgraddiagrams'),
+			@id_sysdiagrams = object_id(N'dbo.sysdiagrams'),
+			@id_helpdiagrams = object_id(N'dbo.sp_helpdiagrams'),
+			@id_helpdiagramdefinition = object_id(N'dbo.sp_helpdiagramdefinition'),
+			@id_creatediagram = object_id(N'dbo.sp_creatediagram'),
+			@id_renamediagram = object_id(N'dbo.sp_renamediagram'),
+			@id_alterdiagram = object_id(N'dbo.sp_alterdiagram'), 
+			@id_dropdiagram = object_id(N'dbo.sp_dropdiagram')
+
+		if @id_upgraddiagrams is not null
+			select @InstalledObjects = @InstalledObjects + 1
+		if @id_sysdiagrams is not null
+			select @InstalledObjects = @InstalledObjects + 2
+		if @id_helpdiagrams is not null
+			select @InstalledObjects = @InstalledObjects + 4
+		if @id_helpdiagramdefinition is not null
+			select @InstalledObjects = @InstalledObjects + 8
+		if @id_creatediagram is not null
+			select @InstalledObjects = @InstalledObjects + 16
+		if @id_renamediagram is not null
+			select @InstalledObjects = @InstalledObjects + 32
+		if @id_alterdiagram  is not null
+			select @InstalledObjects = @InstalledObjects + 64
+		if @id_dropdiagram is not null
+			select @InstalledObjects = @InstalledObjects + 128
+		
+		return @InstalledObjects 
+	END
+	
+GO
+/****** Object:  Table [dbo].[Answer]    Script Date: 8/22/2017 10:54:48 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Answer](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[value] [bit] NOT NULL,
+	[question_id] [int] NOT NULL,
+	[travel_id] [int] NOT NULL,
+ CONSTRAINT [PK__Answer__3213E83FEC5891B2] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+
+GO
+/****** Object:  Table [dbo].[AppUser]    Script Date: 8/22/2017 10:54:49 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AppUser](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[username] [varchar](64) NOT NULL,
+	[password] [varchar](16) NOT NULL,
+	[admin] [bit] NOT NULL,
+	[company_id] [int] NULL,
+	[active] [bit] NOT NULL,
+	[access_token] [varchar](32) NULL,
+ CONSTRAINT [PK__User__3213E83F4E8774DF] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+
+GO
+/****** Object:  Table [dbo].[Company]    Script Date: 8/22/2017 10:54:49 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Company](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](64) NOT NULL,
+	[code] [int] NOT NULL,
+	[active] [bit] NOT NULL,
+	[speed_limit] [float] NOT NULL,
+ CONSTRAINT [PK__Company__3213E83F73860100] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+
+GO
+/****** Object:  Table [dbo].[Log]    Script Date: 8/22/2017 10:54:49 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Log](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[latitude] [decimal](8, 6) NOT NULL,
+	[longitude] [decimal](9, 6) NOT NULL,
+	[date] [datetime] NULL,
+	[travel_id] [int] NOT NULL,
+ CONSTRAINT [PK__Log__3213E83FC9AD0D9C] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+
+GO
+/****** Object:  Table [dbo].[Question]    Script Date: 8/22/2017 10:54:49 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Question](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[title] [varchar](255) NOT NULL,
+	[active] [bit] NOT NULL,
+	[display_order] [int] NOT NULL,
+ CONSTRAINT [PK__Question__3213E83F442CF39F] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+
+GO
+/****** Object:  Table [dbo].[sysdiagrams]    Script Date: 8/22/2017 10:54:50 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[sysdiagrams](
+	[name] [sysname] NOT NULL,
+	[principal_id] [int] NOT NULL,
+	[diagram_id] [int] IDENTITY(1,1) NOT NULL,
+	[version] [int] NULL,
+	[definition] [varbinary](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[diagram_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON),
+ CONSTRAINT [UK_principal_name] UNIQUE NONCLUSTERED 
+(
+	[principal_id] ASC,
+	[name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+
+GO
+/****** Object:  Table [dbo].[Travel]    Script Date: 8/22/2017 10:54:50 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Travel](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[user_id] [int] NOT NULL,
+	[date] [datetime] NOT NULL,
+	[max_speed] [float] NOT NULL,
+	[average_speed] [float] NOT NULL,
+	[distance] [float] NOT NULL,
+	[duration] [varchar](255) NULL,
+ CONSTRAINT [PK__Travel__3213E83F8F1BDCF5] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+
+GO
+ALTER TABLE [dbo].[AppUser] ADD  DEFAULT ((0)) FOR [admin]
+GO
+ALTER TABLE [dbo].[Company] ADD  DEFAULT ((0)) FOR [speed_limit]
+GO
+ALTER TABLE [dbo].[Travel] ADD  DEFAULT ((0)) FOR [max_speed]
+GO
+ALTER TABLE [dbo].[Travel] ADD  DEFAULT ((0)) FOR [average_speed]
+GO
+ALTER TABLE [dbo].[Travel] ADD  DEFAULT ((0)) FOR [distance]
+GO
+ALTER TABLE [dbo].[Answer]  WITH CHECK ADD  CONSTRAINT [fk_Answer_Question_1] FOREIGN KEY([question_id])
+REFERENCES [dbo].[Question] ([id])
+GO
+ALTER TABLE [dbo].[Answer] CHECK CONSTRAINT [fk_Answer_Question_1]
+GO
+ALTER TABLE [dbo].[Answer]  WITH CHECK ADD  CONSTRAINT [fk_Answer_Travel_1] FOREIGN KEY([travel_id])
+REFERENCES [dbo].[Travel] ([id])
+GO
+ALTER TABLE [dbo].[Answer] CHECK CONSTRAINT [fk_Answer_Travel_1]
+GO
+ALTER TABLE [dbo].[AppUser]  WITH CHECK ADD  CONSTRAINT [fk_User_Company_1] FOREIGN KEY([company_id])
+REFERENCES [dbo].[Company] ([id])
+GO
+ALTER TABLE [dbo].[AppUser] CHECK CONSTRAINT [fk_User_Company_1]
+GO
+ALTER TABLE [dbo].[Log]  WITH CHECK ADD  CONSTRAINT [fk_Log_Travel_1] FOREIGN KEY([travel_id])
+REFERENCES [dbo].[Travel] ([id])
+GO
+ALTER TABLE [dbo].[Log] CHECK CONSTRAINT [fk_Log_Travel_1]
+GO
+ALTER TABLE [dbo].[Travel]  WITH CHECK ADD  CONSTRAINT [fk_Travel_User_1] FOREIGN KEY([user_id])
+REFERENCES [dbo].[AppUser] ([id])
+GO
+ALTER TABLE [dbo].[Travel] CHECK CONSTRAINT [fk_Travel_User_1]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_alterdiagram]    Script Date: 8/22/2017 10:54:50 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+	CREATE PROCEDURE [dbo].[sp_alterdiagram]
+	(
+		@diagramname 	sysname,
+		@owner_id	int	= null,
+		@version 	int,
+		@definition 	varbinary(max)
+	)
+	WITH EXECUTE AS 'dbo'
+	AS
+	BEGIN
+		set nocount on
+	
+		declare @theId 			int
+		declare @retval 		int
+		declare @IsDbo 			int
+		
+		declare @UIDFound 		int
+		declare @DiagId			int
+		declare @ShouldChangeUID	int
+	
+		if(@diagramname is null)
+		begin
+			RAISERROR ('Invalid ARG', 16, 1)
+			return -1
+		end
+	
+		execute as caller;
+		select @theId = DATABASE_PRINCIPAL_ID();	 
+		select @IsDbo = IS_MEMBER(N'db_owner'); 
+		if(@owner_id is null)
+			select @owner_id = @theId;
+		revert;
+	
+		select @ShouldChangeUID = 0
+		select @DiagId = diagram_id, @UIDFound = principal_id from dbo.sysdiagrams where principal_id = @owner_id and name = @diagramname 
+		
+		if(@DiagId IS NULL or (@IsDbo = 0 and @theId <> @UIDFound))
+		begin
+			RAISERROR ('Diagram does not exist or you do not have permission.', 16, 1);
+			return -3
+		end
+	
+		if(@IsDbo <> 0)
+		begin
+			if(@UIDFound is null or USER_NAME(@UIDFound) is null) -- invalid principal_id
+			begin
+				select @ShouldChangeUID = 1 ;
+			end
+		end
+
+		-- update dds data			
+		update dbo.sysdiagrams set definition = @definition where diagram_id = @DiagId ;
+
+		-- change owner
+		if(@ShouldChangeUID = 1)
+			update dbo.sysdiagrams set principal_id = @theId where diagram_id = @DiagId ;
+
+		-- update dds version
+		if(@version is not null)
+			update dbo.sysdiagrams set version = @version where diagram_id = @DiagId ;
+
+		return 0
+	END
+	
+GO
+/****** Object:  StoredProcedure [dbo].[sp_creatediagram]    Script Date: 8/22/2017 10:54:50 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+	CREATE PROCEDURE [dbo].[sp_creatediagram]
+	(
+		@diagramname 	sysname,
+		@owner_id		int	= null, 	
+		@version 		int,
+		@definition 	varbinary(max)
+	)
+	WITH EXECUTE AS 'dbo'
+	AS
+	BEGIN
+		set nocount on
+	
+		declare @theId int
+		declare @retval int
+		declare @IsDbo	int
+		declare @userName sysname
+		if(@version is null or @diagramname is null)
+		begin
+			RAISERROR (N'E_INVALIDARG', 16, 1);
+			return -1
+		end
+	
+		execute as caller;
+		select @theId = DATABASE_PRINCIPAL_ID(); 
+		select @IsDbo = IS_MEMBER(N'db_owner');
+		revert; 
+		
+		if @owner_id is null
+		begin
+			select @owner_id = @theId;
+		end
+		else
+		begin
+			if @theId <> @owner_id
+			begin
+				if @IsDbo = 0
+				begin
+					RAISERROR (N'E_INVALIDARG', 16, 1);
+					return -1
+				end
+				select @theId = @owner_id
+			end
+		end
+		-- next 2 line only for test, will be removed after define name unique
+		if EXISTS(select diagram_id from dbo.sysdiagrams where principal_id = @theId and name = @diagramname)
+		begin
+			RAISERROR ('The name is already used.', 16, 1);
+			return -2
+		end
+	
+		insert into dbo.sysdiagrams(name, principal_id , version, definition)
+				VALUES(@diagramname, @theId, @version, @definition) ;
+		
+		select @retval = @@IDENTITY 
+		return @retval
+	END
+	
+GO
+/****** Object:  StoredProcedure [dbo].[sp_dropdiagram]    Script Date: 8/22/2017 10:54:50 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+	CREATE PROCEDURE [dbo].[sp_dropdiagram]
+	(
+		@diagramname 	sysname,
+		@owner_id	int	= null
+	)
+	WITH EXECUTE AS 'dbo'
+	AS
+	BEGIN
+		set nocount on
+		declare @theId 			int
+		declare @IsDbo 			int
+		
+		declare @UIDFound 		int
+		declare @DiagId			int
+	
+		if(@diagramname is null)
+		begin
+			RAISERROR ('Invalid value', 16, 1);
+			return -1
+		end
+	
+		EXECUTE AS CALLER;
+		select @theId = DATABASE_PRINCIPAL_ID();
+		select @IsDbo = IS_MEMBER(N'db_owner'); 
+		if(@owner_id is null)
+			select @owner_id = @theId;
+		REVERT; 
+		
+		select @DiagId = diagram_id, @UIDFound = principal_id from dbo.sysdiagrams where principal_id = @owner_id and name = @diagramname 
+		if(@DiagId IS NULL or (@IsDbo = 0 and @UIDFound <> @theId))
+		begin
+			RAISERROR ('Diagram does not exist or you do not have permission.', 16, 1)
+			return -3
+		end
+	
+		delete from dbo.sysdiagrams where diagram_id = @DiagId;
+	
+		return 0;
+	END
+	
+GO
+/****** Object:  StoredProcedure [dbo].[sp_helpdiagramdefinition]    Script Date: 8/22/2017 10:54:50 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+	CREATE PROCEDURE [dbo].[sp_helpdiagramdefinition]
+	(
+		@diagramname 	sysname,
+		@owner_id	int	= null 		
+	)
+	WITH EXECUTE AS N'dbo'
+	AS
+	BEGIN
+		set nocount on
+
+		declare @theId 		int
+		declare @IsDbo 		int
+		declare @DiagId		int
+		declare @UIDFound	int
+	
+		if(@diagramname is null)
+		begin
+			RAISERROR (N'E_INVALIDARG', 16, 1);
+			return -1
+		end
+	
+		execute as caller;
+		select @theId = DATABASE_PRINCIPAL_ID();
+		select @IsDbo = IS_MEMBER(N'db_owner');
+		if(@owner_id is null)
+			select @owner_id = @theId;
+		revert; 
+	
+		select @DiagId = diagram_id, @UIDFound = principal_id from dbo.sysdiagrams where principal_id = @owner_id and name = @diagramname;
+		if(@DiagId IS NULL or (@IsDbo = 0 and @UIDFound <> @theId ))
+		begin
+			RAISERROR ('Diagram does not exist or you do not have permission.', 16, 1);
+			return -3
+		end
+
+		select version, definition FROM dbo.sysdiagrams where diagram_id = @DiagId ; 
+		return 0
+	END
+	
+GO
+/****** Object:  StoredProcedure [dbo].[sp_helpdiagrams]    Script Date: 8/22/2017 10:54:50 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+	CREATE PROCEDURE [dbo].[sp_helpdiagrams]
+	(
+		@diagramname sysname = NULL,
+		@owner_id int = NULL
+	)
+	WITH EXECUTE AS N'dbo'
+	AS
+	BEGIN
+		DECLARE @user sysname
+		DECLARE @dboLogin bit
+		EXECUTE AS CALLER;
+			SET @user = USER_NAME();
+			SET @dboLogin = CONVERT(bit,IS_MEMBER('db_owner'));
+		REVERT;
+		SELECT
+			[Database] = DB_NAME(),
+			[Name] = name,
+			[ID] = diagram_id,
+			[Owner] = USER_NAME(principal_id),
+			[OwnerID] = principal_id
+		FROM
+			sysdiagrams
+		WHERE
+			(@dboLogin = 1 OR USER_NAME(principal_id) = @user) AND
+			(@diagramname IS NULL OR name = @diagramname) AND
+			(@owner_id IS NULL OR principal_id = @owner_id)
+		ORDER BY
+			4, 5, 1
+	END
+	
+GO
+/****** Object:  StoredProcedure [dbo].[sp_renamediagram]    Script Date: 8/22/2017 10:54:50 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+	CREATE PROCEDURE [dbo].[sp_renamediagram]
+	(
+		@diagramname 		sysname,
+		@owner_id		int	= null,
+		@new_diagramname	sysname
+	
+	)
+	WITH EXECUTE AS 'dbo'
+	AS
+	BEGIN
+		set nocount on
+		declare @theId 			int
+		declare @IsDbo 			int
+		
+		declare @UIDFound 		int
+		declare @DiagId			int
+		declare @DiagIdTarg		int
+		declare @u_name			sysname
+		if((@diagramname is null) or (@new_diagramname is null))
+		begin
+			RAISERROR ('Invalid value', 16, 1);
+			return -1
+		end
+	
+		EXECUTE AS CALLER;
+		select @theId = DATABASE_PRINCIPAL_ID();
+		select @IsDbo = IS_MEMBER(N'db_owner'); 
+		if(@owner_id is null)
+			select @owner_id = @theId;
+		REVERT;
+	
+		select @u_name = USER_NAME(@owner_id)
+	
+		select @DiagId = diagram_id, @UIDFound = principal_id from dbo.sysdiagrams where principal_id = @owner_id and name = @diagramname 
+		if(@DiagId IS NULL or (@IsDbo = 0 and @UIDFound <> @theId))
+		begin
+			RAISERROR ('Diagram does not exist or you do not have permission.', 16, 1)
+			return -3
+		end
+	
+		-- if((@u_name is not null) and (@new_diagramname = @diagramname))	-- nothing will change
+		--	return 0;
+	
+		if(@u_name is null)
+			select @DiagIdTarg = diagram_id from dbo.sysdiagrams where principal_id = @theId and name = @new_diagramname
+		else
+			select @DiagIdTarg = diagram_id from dbo.sysdiagrams where principal_id = @owner_id and name = @new_diagramname
+	
+		if((@DiagIdTarg is not null) and  @DiagId <> @DiagIdTarg)
+		begin
+			RAISERROR ('The name is already used.', 16, 1);
+			return -2
+		end		
+	
+		if(@u_name is null)
+			update dbo.sysdiagrams set [name] = @new_diagramname, principal_id = @theId where diagram_id = @DiagId
+		else
+			update dbo.sysdiagrams set [name] = @new_diagramname where diagram_id = @DiagId
+		return 0
+	END
+	
+GO
+/****** Object:  StoredProcedure [dbo].[sp_upgraddiagrams]    Script Date: 8/22/2017 10:54:50 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+	CREATE PROCEDURE [dbo].[sp_upgraddiagrams]
+	AS
+	BEGIN
+		IF OBJECT_ID(N'dbo.sysdiagrams') IS NOT NULL
+			return 0;
+	
+		CREATE TABLE dbo.sysdiagrams
+		(
+			name sysname NOT NULL,
+			principal_id int NOT NULL,	-- we may change it to varbinary(85)
+			diagram_id int PRIMARY KEY IDENTITY,
+			version int,
+	
+			definition varbinary(max)
+			CONSTRAINT UK_principal_name UNIQUE
+			(
+				principal_id,
+				name
+			)
+		);
+
+
+		/* Add this if we need to have some form of extended properties for diagrams */
+		/*
+		IF OBJECT_ID(N'dbo.sysdiagram_properties') IS NULL
+		BEGIN
+			CREATE TABLE dbo.sysdiagram_properties
+			(
+				diagram_id int,
+				name sysname,
+				value varbinary(max) NOT NULL
+			)
+		END
+		*/
+
+		IF OBJECT_ID(N'dbo.dtproperties') IS NOT NULL
+		begin
+			insert into dbo.sysdiagrams
+			(
+				[name],
+				[principal_id],
+				[version],
+				[definition]
+			)
+			select	 
+				convert(sysname, dgnm.[uvalue]),
+				DATABASE_PRINCIPAL_ID(N'dbo'),			-- will change to the sid of sa
+				0,							-- zero for old format, dgdef.[version],
+				dgdef.[lvalue]
+			from dbo.[dtproperties] dgnm
+				inner join dbo.[dtproperties] dggd on dggd.[property] = 'DtgSchemaGUID' and dggd.[objectid] = dgnm.[objectid]	
+				inner join dbo.[dtproperties] dgdef on dgdef.[property] = 'DtgSchemaDATA' and dgdef.[objectid] = dgnm.[objectid]
+				
+			where dgnm.[property] = 'DtgSchemaNAME' and dggd.[uvalue] like N'_EA3E6268-D998-11CE-9454-00AA00A3F36E_' 
+			return 2;
+		end
+		return 1;
+	END
+	
+GO
+USE [master]
+GO
+ALTER DATABASE [trayectoseguro] SET  READ_WRITE 
+GO
